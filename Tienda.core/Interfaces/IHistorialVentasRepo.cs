2a95c0efe27fb2f6754dc00a6489883f0d8f95cd
JoseUpia <upia915@gmail.com>
2021-04-26 23:39:57 -0400

    Subiendo HistorialVentasRepo

--- Tienda.core/Interfaces/IHistorialVentasRepo.cs
+++ Tienda.core/Interfaces/IHistorialVentasRepo.cs
@@ -8,8 +8,8 @@ namespace Tienda.core.Interfaces
 {
     public interface IHistorialVentasRepo
     {
-        Task<IEnumerable<IHistorialVentasRepo>> GetHistorialVentas();
-        Task<IHistorialVentasRepo> GetHistorialVentas(int id);
+        Task<IEnumerable<HistorialVentas>> GetHistorialVentas();
+        Task<HistorialVentas> GetHistorialVentas(int id);
         Task InsetHistorialVentas(HistorialVentas historialVentas);
         Task<bool> UpdateHistorialVentas(HistorialVentas historialVentas);
         Task<bool> DeleteHistorialVentas(int id);
