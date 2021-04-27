7dfbd9cc9c7feafb8b97db15ea3d6ca80907b816
JoseUpia <upia915@gmail.com>
2021-04-26 23:16:44 -0400

    Subiendo HistorialVentasDto y VendedorDto

---
+++ Tienda.core/DTOs/HistorialVentasDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tienda.core.DTOs
+{
+    public class HistorialVentasDto
+    {
+        public int IdHistorial { get; set; }
+        public int? IdCliente { get; set; }
+        public int? IdVendedor { get; set; }
+        public int? IdProducto { get; set; }
+        public DateTime? Fecha { get; set; }
+    }
+}
